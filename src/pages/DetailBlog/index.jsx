01b66d93596a338ee571b270376efe265ea4615b
florensiuspancagati <florensiuspancagati@gmail.com>
2025-08-11 20:49:15 +0700

    Styling in Detail Blog Page

--- src/pages/DetailBlog/index.jsx
+++ src/pages/DetailBlog/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ICReact, loginbg } from '../../assets'
+import './index.scss'
 
 const DetailBlog = () => {
   return (
-    <div>
-        <p>Content Detail Blog Page</p>
+    <div className='detail-blog-wrapper'>
+      <img className='img-cover' src={loginbg} alt="thumb" />
+        <p className='blog-title'>Title Blog</p>
+        <p className='blog-author'>Author - Date Post</p>
+        <p className='blog-body'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos voluptatibus cum expedita, voluptates repellendus sit dolor minima sint eum beatae nam consequuntur fugit error animi inventore adipisci accusamus voluptatum doloremque?</p>
     </div>
   )
 }
